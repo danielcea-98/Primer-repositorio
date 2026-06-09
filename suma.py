ea4361d923fb99209e7f7f6abf3a0958a559f2a5
Daniel Alejandro <dan.ceac@duocuc.cl>
2026-06-09 16:19:32 -0400

    Código actualizado
    se agrega al código una función que detecta el número mayor de la lista

--- suma.py
+++ suma.py
@@ -2,8 +2,22 @@ numeros = [2, 4, 6, 8]
 
 def suma(lista):
     total = 0
+    mayor = numeros[0]
+
     for numero in numeros:
         total += numero
     return total
 
+def mayor(lista):
+    
+    mayor = numeros[0]
+
+    for numero in numeros:
+        if numero > mayor:
+            mayor = numero
+    return mayor
+
+
+
 print(suma(numeros))
+print(mayor(numeros))
